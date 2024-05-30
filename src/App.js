fb5b94aaa40587ea4471a23234972e09935321d9
lyrawb@gmail.com <lyrawb@gmail.com>
2024-05-30 17:18:33 +0600

    1dz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom';
+import Header from './components/Header/Header';
+import HomePage from './components/Main/HomePage';
+import Page1 from './components/Main/Page1';
+import Page2 from './components/Main/Page2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+    return (
+        <Router>
+            <Header />
+            
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/page1" element={<Page1 />} />
+                <Route path="/page2" element={<Page2 />} />
+            </Routes>
+        </Router>
+    );
+};
